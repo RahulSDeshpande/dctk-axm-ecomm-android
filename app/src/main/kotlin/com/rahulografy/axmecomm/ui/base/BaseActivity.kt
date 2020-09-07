f6cd601b0a6061bb70a872bfc0515a923474556d
AndroCurious <rahul.deshpande92@gmail.com>
2020-09-08 03:06:15 +0400

    Create base activity, fragment & view-model

---
+++ app/src/main/kotlin/com/rahulografy/axmecomm/ui/base/BaseActivity.kt
@@ -0,0 +1,5 @@
+package com.rahulografy.axmecomm.ui.base
+
+import dagger.android.support.DaggerAppCompatActivity
+
+abstract class BaseActivity : DaggerAppCompatActivity()
---
+++ app/src/main/kotlin/com/rahulografy/axmecomm/ui/base/BaseFragment.kt
@@ -0,0 +1,5 @@
+package com.rahulografy.axmecomm.ui.base
+
+import dagger.android.support.DaggerFragment
+
+abstract class BaseFragment : DaggerFragment()
