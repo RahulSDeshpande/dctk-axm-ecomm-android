db2680a3550346cbf98f1334570d29f36f5d8ba8
AndroCurious <rahul.deshpande92@gmail.com>
2020-09-09 02:42:29 +0400

    [MVVMization] Create HomeActivityViewModel

--- app/src/main/kotlin/com/rahulografy/axmecomm/ui/main/home/activity/HomeActivity.kt
+++ app/src/main/kotlin/com/rahulografy/axmecomm/ui/main/home/activity/HomeActivity.kt
@@ -1,15 +1,18 @@
 package com.rahulografy.axmecomm.ui.main.home.activity
 
-import android.os.Bundle
+import com.rahulografy.axmecomm.BR
 import com.rahulografy.axmecomm.R
 import com.rahulografy.axmecomm.databinding.ActivityMainBinding
-import com.rahulografy.axmecomm.ui.base.BaseActivity
+import com.rahulografy.axmecomm.ui.base.view.BaseActivity
 
-class HomeActivity : BaseActivity<ActivityMainBinding>() {
+class HomeActivity : BaseActivity<ActivityMainBinding, HomeActivityViewModel>() {
 
     override val layoutRes: Int get() = R.layout.activity_main
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override val viewModelClass get() = HomeActivityViewModel::class.java
+
+    override val bindingVariable get() = BR.viewModel
+
+    override fun initUi() {
     }
 }
---
+++ app/src/main/kotlin/com/rahulografy/axmecomm/ui/main/home/activity/HomeActivityViewModel.kt
@@ -0,0 +1,6 @@
+package com.rahulografy.axmecomm.ui.main.home.activity
+
+import com.rahulografy.axmecomm.ui.base.view.BaseViewModel
+import javax.inject.Inject
+
+class HomeActivityViewModel @Inject constructor() : BaseViewModel()
