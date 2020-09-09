8e2bf046d9b86846a973e0601418c0d36b8451ae
AndroCurious <rahul.deshpande92@gmail.com>
2020-09-10 00:36:12 +0400

    Integrate ViewPager & TabLayout in HomeFragment & update layouts

--- app/src/main/kotlin/com/rahulografy/axmecomm/ui/main/home/fragment/HomeFragment.kt
+++ app/src/main/kotlin/com/rahulografy/axmecomm/ui/main/home/fragment/HomeFragment.kt
@@ -1,12 +1,15 @@
 package com.rahulografy.axmecomm.ui.main.home.fragment
 
 import androidx.lifecycle.Observer
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayoutMediator
 import com.rahulografy.axmecomm.BR
 import com.rahulografy.axmecomm.R
 import com.rahulografy.axmecomm.databinding.FragmentHomeBinding
 import com.rahulografy.axmecomm.ui.base.view.BaseFragment
-import com.rahulografy.axmecomm.ui.main.home.fragment.adapter.ProductAdapter
+import com.rahulografy.axmecomm.ui.main.home.ProductFragmentAdapter
 import com.rahulografy.axmecomm.util.ext.toast
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : BaseFragment<FragmentHomeBinding, HomeFragmentViewModel>() {
 
@@ -16,26 +19,58 @@ class HomeFragment : BaseFragment<FragmentHomeBinding, HomeFragmentViewModel>() 
 
     override val bindingVariable = BR.viewModel
 
-    private lateinit var productAdapter: ProductAdapter
-
     override fun initUi() {
 
-        productAdapter = ProductAdapter(homeFragmentViewModel = viewModel)
-
-        viewDataBinding.productAdapter = productAdapter
-        viewDataBinding.executePendingBindings()
-
         viewModel.getProducts()
+    }
+
+    override fun initSharedViewModelObservers() {
 
         viewModel
-            .productClickEvent
+            .productsManager
+            .mapCategoryWiseProducts
             .observe(
                 lifecycleOwner = this,
-                observer = Observer { productItem ->
-                    productItem?.let {
-                        toast(text = "Phone: ${productItem.phone}")
-                    }
+                observer = Observer {
+                    initViewPager(categories = it?.keys)
+                    viewModel.isApiCallInProgress.set(false)
                 }
             )
+    }
+
+    private fun initViewPager(categories: Collection<String>?) {
+
+        if (!categories.isNullOrEmpty()) {
+
+            val productFragmentAdapter =
+                ProductFragmentAdapter(
+                    categories = categories.toList(),
+                    childFragmentManager = childFragmentManager,
+                    lifecycle = lifecycle
+                )
+
+            viewPager2Home?.apply {
+                registerOnPageChangeCallback(
+                    object : ViewPager2.OnPageChangeCallback() {
+                        override fun onPageSelected(position: Int) {
+                            toast("Selected page position: $position")
+                        }
+                    }
+                )
+
+                adapter = productFragmentAdapter
+
+                offscreenPageLimit = categories.size
+            }
+
+            TabLayoutMediator(
+                tabLayoutHome,
+                viewPager2Home
+            ) { tab, position ->
+                tab.text = categories.toList()[position]
+            }.attach()
+        } else {
+            toast("API error occurred while fetching products.")
+        }
     }
 }
--- app/src/main/kotlin/com/rahulografy/axmecomm/ui/main/home/fragment/HomeFragmentViewModel.kt
+++ app/src/main/kotlin/com/rahulografy/axmecomm/ui/main/home/fragment/HomeFragmentViewModel.kt
@@ -1,56 +1,19 @@
 package com.rahulografy.axmecomm.ui.main.home.fragment
 
 import androidx.databinding.ObservableBoolean
-import androidx.databinding.ObservableField
-import com.rahulografy.axmecomm.data.repository.products.ProductsRepository
 import com.rahulografy.axmecomm.ui.base.view.BaseViewModel
-import com.rahulografy.axmecomm.ui.main.home.fragment.mapper.ProductsMapper
-import com.rahulografy.axmecomm.ui.main.home.fragment.model.ProductItem
-import com.rahulografy.axmecomm.ui.main.home.fragment.model.Products
-import com.rahulografy.axmecomm.util.SingleLiveEvent
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.schedulers.Schedulers
+import com.rahulografy.axmecomm.ui.main.home.ProductsManager
 import javax.inject.Inject
 
-class HomeFragmentViewModel @Inject constructor(
-    private val productsRepository: ProductsRepository,
-    private val productsMapper: ProductsMapper
+class HomeFragmentViewModel
+@Inject constructor(
+    val productsManager: ProductsManager
 ) : BaseViewModel() {
 
     val isApiCallInProgress = ObservableBoolean(false)
 
-    val products = ObservableField<Products>()
-
-    val productClickEvent = SingleLiveEvent<ProductItem>()
-
-    fun refresh() {
-        getProducts()
-    }
-
     fun getProducts(forceApi: Boolean = false) {
-        if (forceApi || products.get()?.products.isNullOrEmpty()) {
-            addDisposable(
-                disposable = productsRepository
-                    .getProducts()
-                    .subscribeOn(Schedulers.io())
-                    .observeOn(AndroidSchedulers.mainThread())
-                    .doOnSubscribe {
-                        isApiCallInProgress.set(true)
-                    }
-                    .subscribe({
-                        isApiCallInProgress.set(false)
-                        products.set(productsMapper.map(it))
-                    }, { error ->
-                        isApiCallInProgress.set(false)
-                        error.printStackTrace()
-                    })
-            )
-        } else {
-            products.notifyChange()
-        }
-    }
-
-    fun openProductDetails(productItem: ProductItem) {
-        productClickEvent.value = productItem
+        isApiCallInProgress.set(true)
+        productsManager.getProducts(forceApi = forceApi)
     }
 }
