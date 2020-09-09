e5a99524080b9914a7f27f6febb197a77e456e1f
AndroCurious <rahul.deshpande92@gmail.com>
2020-09-09 12:27:01 +0400

    Add Picasso lib dep & create exts for picasso & alert dialog

--- app/src/main/kotlin/com/rahulografy/axmecomm/util/ext/ViewExt.kt
+++ app/src/main/kotlin/com/rahulografy/axmecomm/util/ext/ViewExt.kt
@@ -1,11 +1,17 @@
 package com.rahulografy.axmecomm.util.ext
 
 import android.content.Context
+import android.widget.ImageView
 import android.widget.Toast
+import androidx.annotation.DrawableRes
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.rahulografy.axmecomm.R
+import com.squareup.picasso.Picasso
 import jp.wasabeef.recyclerview.animators.FadeInDownAnimator
 import jp.wasabeef.recyclerview.animators.FadeInRightAnimator
 import jp.wasabeef.recyclerview.animators.FadeInUpAnimator
@@ -85,3 +91,35 @@ fun Context.toast(text: String, duration: Int = Toast.LENGTH_LONG) {
 fun Fragment.toast(text: String, duration: Int = Toast.LENGTH_LONG) {
     context?.toast(text = text, duration = duration)
 }
+
+fun Context.alert(
+    title: String,
+    message: String,
+    @DrawableRes icon: Int? = null,
+    positiveButton: String = getString(R.string.ok)
+): AlertDialog =
+    AlertDialog
+        .Builder(this)
+        .setTitle(title)
+        .setIcon(icon ?: android.R.drawable.screen_background_light_transparent)
+        .setMessage(message)
+        .setPositiveButton(
+            positiveButton, null
+        ).show()
+
+fun ImageView?.picasso(
+    url: String?,
+    @DrawableRes placeHolderDrawableRes: Int = R.drawable.ic_placeholder_1
+) = Picasso.get().load(url).placeholder(placeHolderDrawableRes).into(this)
+
+fun ImageView?.picasso(@DrawableRes drawableRes: Int = R.drawable.ic_placeholder_1) =
+    Picasso.get().load(drawableRes).into(this)
+
+fun ImageView.glide(
+    url: String?,
+    @DrawableRes placeHolderDrawableRes: Int = R.drawable.ic_placeholder_1
+) =
+    Glide
+        .with(context)
+        .load(url)
+        .into(this)
